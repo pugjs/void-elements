712ebc3506b93f0d357b6a97d1e2077c9cafdb61
Timothy Gu <timothygu99@gmail.com>
2015-01-28 23:14:41 -0800

    Update test to README.md
    not like it really matters...

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
 var voidElements = require('../');
-assert(!voidElements.span, '<span> is not a void element');
-assert(voidElements.img, '<img> is a void element');
+assert(!voidElements['span'], '<span> is not a void element');
+assert( voidElements['img'] , '<img> is a void element');
 console.log('tests passed');
