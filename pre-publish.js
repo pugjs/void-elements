cbda7bbe9be9da1018f91fe4554e669f6fe3e50a
ForbesLindesay <forbes@lindesay.co.uk>
2017-01-18 15:22:18 +0000

    Use official whatwg spec
    This required using jsdom rather than cheerio as the parser because the
    whatwg document omits closing tags in any places where they will be
    automatically inserted by the browser.  jsdom is a little slower, but
    should offer more consistent behaviour, and this is only run at build
    time.

--- pre-publish.js
+++ pre-publish.js
@@ -1,24 +1,33 @@
-var cheerio = require('cheerio');
+var jsdom = require('jsdom');
 var request = require('request-promise');
 
-request('https://w3c.github.io/html/syntax.html')
+request('https://html.spec.whatwg.org/multipage/syntax.html')
   .then(function (str) {
-    var $ = cheerio.load(str);
-    var codes = $('dfn#void-elements')
-                .parent()
-                .next()
-                .text()
-                .replace(/\s/gm,'')
-                .split(",")
-                .reduce(function (obj, code) {
-                  obj[code] = true;
-                  return obj;
-                }, {});
+    jsdom.env(
+      str,
+      (err, window) => {
+        if (err) {
+          throw err;
+        }
+        const document = window.document;
+        var codes = document.querySelector('dfn#void-elements')
+                    .parentNode
+                    .nextElementSibling
+                    .textContent
+                    .replace(/\s/gm,'')
+                    .split(",")
+                    .reduce(function (obj, code) {
+                      obj[code] = true;
+                      return obj;
+                    }, {});
 
-    console.log('/**');
-    console.log(' * This file automatically generated from `pre-publish.js`.');
-    console.log(' * Do not manually edit.');
-    console.log(' */');
-    console.log();
-    console.log('module.exports = %s;', JSON.stringify(codes, null, 2));
+        console.log('/**');
+        console.log(' * This file automatically generated from `pre-publish.js`.');
+        console.log(' * Do not manually edit.');
+        console.log(' */');
+        console.log();
+        console.log('module.exports = %s;', JSON.stringify(codes, null, 2));
+        window.close();
+      }
+    );
   });
