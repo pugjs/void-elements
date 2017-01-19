59e704875907f0ee0c2546dabe780695e7cc3b87
Forbes Lindesay <forbes@lindesay.co.uk>
2017-01-19 09:15:46 +0000

    Update manually but test that current matches latest

--- test/index.js
+++ test/index.js
@@ -1,5 +1,13 @@
-var assert = require('assert');
-var voidElements = require('../');
+const assert = require('assert');
+const voidElements = require('../');
 assert(!voidElements['span'], '<span> is not a void element');
 assert( voidElements['img'] , '<img> is a void element');
+
+const latestVoidElements = require('./latest');
+assert.deepEqual(
+  voidElements,
+  latestVoidElements,
+  'The list of void elements has changed, run "npm run update" to update this list.'
+);
+
 console.log('tests passed');
