6a74ee93765ec8bae24573ad7d08f6de4a6f19bc
Forbes Lindesay <forbes@lindesay.co.uk>
2017-01-19 09:10:33 +0000

    Remove node v5 from travis and use more ES6

--- pre-publish.js
+++ pre-publish.js
@@ -1,33 +1,31 @@
-var jsdom = require('jsdom');
-var request = require('request-promise');
+'use strict';
 
-request('https://html.spec.whatwg.org/multipage/syntax.html')
-  .then(function (str) {
-    jsdom.env(
-      str,
-      (err, window) => {
-        if (err) {
-          throw err;
-        }
-        const document = window.document;
-        var codes = document.querySelector('dfn#void-elements')
-                    .parentNode
-                    .nextElementSibling
-                    .textContent
-                    .replace(/\s/gm,'')
-                    .split(",")
-                    .reduce(function (obj, code) {
-                      obj[code] = true;
-                      return obj;
-                    }, {});
+const jsdom = require('jsdom');
+const request = require('request-promise');
 
-        console.log('/**');
-        console.log(' * This file automatically generated from `pre-publish.js`.');
-        console.log(' * Do not manually edit.');
-        console.log(' */');
-        console.log();
-        console.log('module.exports = %s;', JSON.stringify(codes, null, 2));
-        window.close();
-      }
-    );
+request('https://html.spec.whatwg.org/multipage/syntax.html').then(str => {
+  jsdom.env(str, (err, window) => {
+    if (err) {
+      throw err;
+    }
+    const document = window.document;
+    const codes = document.querySelector('dfn#void-elements')
+                .parentNode
+                .nextElementSibling
+                .textContent
+                .replace(/\s/gm,'')
+                .split(",")
+                .reduce((obj, code) => {
+                  obj[code] = true;
+                  return obj;
+                }, {});
+
+    console.log('/**');
+    console.log(' * This file automatically generated from `pre-publish.js`.');
+    console.log(' * Do not manually edit.');
+    console.log(' */');
+    console.log();
+    console.log('module.exports = %s;', JSON.stringify(codes, null, 2));
+    window.close();
   });
+});
